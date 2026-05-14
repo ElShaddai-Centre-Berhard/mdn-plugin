c30689db321ce84eb2f00378c27a50c3fb8b705b
wobiljames <wobiljames55@gmail.com>
2026-05-14 15:16:36 +0800

    create plugin base

---
+++ mdn-plugin.php
@@ -0,0 +1,9 @@
+/*
+ * Plugin Name:       Malaysia Diaspora Network Directoty
+ * Plugin URI:        https://github.com/ElShaddai-Centre-Berhard/mdn-plugin
+ * Description:       A plugin built with the idea of making a simple management tool for Malaysia Diaspora Network Directory
+ * Version:           1.0
+ * Author:            James Wobil
+ * Requires at least: 6.4    
+ * Author URI:        https://github.com/wobiljames
+ */
