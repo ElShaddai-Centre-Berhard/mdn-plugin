e3019e663b2d049ad239e66145c50715c59f6fa5
wobiljames <wobiljames55@gmail.com>
2026-05-14 16:07:03 +0800

    updated the base php

--- mdn-plugin.php
+++ mdn-plugin.php
@@ -1,9 +1,27 @@
+<?php
 /*
- * Plugin Name:       Malaysia Diaspora Network Directoty
+ * Plugin Name:       Malaysia Diaspora Network Directory
  * Plugin URI:        https://github.com/ElShaddai-Centre-Berhard/mdn-plugin
  * Description:       A plugin built with the idea of making a simple management tool for Malaysia Diaspora Network Directory
- * Version:           1.0
+ * Version:           1.0.0
  * Author:            James Wobil
- * Requires at least: 6.4    
+ * Requires at least: 6.4
  * Author URI:        https://github.com/wobiljames
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
+
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$mdn_update_checker = PucFactory::buildUpdateChecker(
+	'https://github.com/ElShaddai-Centre-Berhard/mdn-plugin/',
+	__FILE__,
+	'mdn-plugin'
+);
+
+$mdn_update_checker->setBranch( 'main' );
+$mdn_update_checker->getVcsApi()->enableReleaseAssets();
