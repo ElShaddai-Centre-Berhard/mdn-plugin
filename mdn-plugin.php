048299230cfc681258498888c0e644827aebccb5
James Wobil <wobiljames55@gmail.com>
2026-05-14 16:20:08 +0800

    Bump version to 1.0.1

--- mdn-plugin.php
+++ mdn-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Malaysia Diaspora Network
  * Plugin URI:        https://github.com/ElShaddai-Centre-Berhard/mdn-plugin
  * Description:       A plugin built with the idea of making a simple management tool for Malaysia Diaspora Network Directory
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            James Wobil
  * Requires at least: 6.4
  * Author URI:        https://github.com/wobiljames
