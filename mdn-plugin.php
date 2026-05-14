1d896a6f3c20c872272d0452a4099888ef96911f
James Wobil <wobiljames55@gmail.com>
2026-05-14 16:08:27 +0800

    Rename plugin from 'Malaysia Diaspora Network Directory' to 'Malaysia Diaspora Network'

--- mdn-plugin.php
+++ mdn-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name:       Malaysia Diaspora Network Directory
+ * Plugin Name:       Malaysia Diaspora Network
  * Plugin URI:        https://github.com/ElShaddai-Centre-Berhard/mdn-plugin
  * Description:       A plugin built with the idea of making a simple management tool for Malaysia Diaspora Network Directory
  * Version:           1.0.0
