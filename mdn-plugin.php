aac2bb5b411905699fc4b18814ee4da7f6c765ec
ecbtechteam <itsupport@ecb.org.my>
2026-05-15 14:47:19 +0800

    Update mdn-plugin.php

--- mdn-plugin.php
+++ mdn-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Malaysia Diaspora Network
  * Plugin URI:        https://github.com/ElShaddai-Centre-Berhard/mdn-plugin
  * Description:       A plugin built with the idea of making a simple management tool for Malaysia Diaspora Network Directory
- * Version:           1.0.3
+ * Version:           1.0.3.1
  * Author:            James Wobil
  * Requires at least: 6.4
  * Author URI:        https://github.com/wobiljames
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'MDN_VERSION', '1.0.3' );
+define( 'MDN_VERSION', '1.0.3.1' );
 define( 'MDN_PLUGIN_FILE', __FILE__ );
 define( 'MDN_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'MDN_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
